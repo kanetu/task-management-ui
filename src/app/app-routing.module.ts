057420ef9acfadd521d6890ae4bb64457b200836
Kane <kanetu731@gmail.com>
2021-10-07 23:47:13 +0700

    Inital project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "task",
+    loadChildren: () => import("./pages/task/task.module").then(m => m.TaskModule)
+  },
+  {
+    path: "project",
+    loadChildren: () => import("./pages/project/project.module").then(m => m.ProjectModule)
+  },
+  {
+    path: "user",
+    loadChildren: () => import("./pages/user/user.module").then(m => m.UserModule)
+  },
+  {
+    path: "schedule",
+    loadChildren: () => import("./pages/schedule/schedule.module").then(m => m.ScheduleModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/task/task.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TaskRoutingModule } from './task-routing.module';
+import {TaskComponent} from './task.component';
+
+
+@NgModule({
+  declarations: [
+    TaskComponent
+  ],
+  imports: [
+    CommonModule,
+    TaskRoutingModule
+  ],
+  exports: [
+    TaskComponent
+  ]
+})
+export class TaskModule { }
